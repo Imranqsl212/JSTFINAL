85a76b8e36f7088e15e5fde31f07f46f90ad0a56
Imranqsl212 <zakirovimran333@gmail.com>
2023-10-11 18:09:54 +0600

    added price filter

---
+++ frontend/src/Components/FilterHotels/FilterHotels.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styles from "./FilterHotels.module.css";
+
+const FilterHotels = ({ sortingOrder, setSortingOrder }) => {
+  const filterOptions = {
+    all: "",
+    cheap: "asc",
+    expensive: "desc",
+  };
+
+  const handleFilterChange = (event) => {
+    setSortingOrder(filterOptions[event.target.id]);
+  };
+
+  const optionKeys = Object.keys(filterOptions);
+
+  return (
+    <div className={styles.mydict}>
+      <div>
+        {optionKeys.map((option) => (
+          <label key={option} htmlFor={option}>
+            <input
+              type="radio"
+              name="filter"
+              id={option}
+              checked={sortingOrder === filterOptions[option]}
+              value={filterOptions[option]}
+              onChange={handleFilterChange}
+            />
+            <span>{option.charAt(0).toUpperCase() + option.slice(1)}</span>
+          </label>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FilterHotels;
--- frontend/src/Pages/Hotels/Hotels.jsx
+++ frontend/src/Pages/Hotels/Hotels.jsx
@@ -1,28 +1,40 @@
 import React, { useState, useEffect } from "react";
 import HotelComponent from "../../Components/HotelsComponents/HotelComponent";
 import axios from "axios";
-import Loader from '../../Components/Loader/Loader'
+import Loader from "../../Components/Loader/Loader";
+import FilterHotels from "../../Components/FilterHotels/FilterHotels";
 
 const Hotels = () => {
   const [data, setData] = useState([]);
+  const [sortingOrder, setSortingOrder] = useState("");
+  console.log(sortingOrder);
   const fetchData = async () => {
     try {
-      const url = "https://651ed40844a3a8aa476913dc.mockapi.io/api/Hotels";
+      const pre = sortingOrder === "" ? "" : `?sortBy=price&order=${sortingOrder}`;
+      const url = `https://651ed40844a3a8aa476913dc.mockapi.io/api/Hotels${pre}`;
+      console.log(url);
       const res = await axios.get(url);
-      console.log(1);
+
       setData(res.data);
     } catch (error) {
       alert(error);
     }
   };
-  useEffect(()=>{
-    fetchData()
-  },[])
+  useEffect(() => {
+    fetchData();
+  }, []);
+  useEffect(() => {
+    fetchData();
+  }, [sortingOrder]);
   if (!data || data.length === 0) {
     return <Loader />;
   }
   return (
     <section>
+      <FilterHotels
+        sortingOrder={sortingOrder}
+        setSortingOrder={setSortingOrder}
+      />
       <HotelComponent data={data} />
     </section>
   );
